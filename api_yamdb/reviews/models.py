d37935e75e9fc013ed6bb1899de7eda536e6d8e3
Святослав Прохоренко <prosvo@yandex.ru>
2022-11-01 07:09:14 +0300

    add/reviews_models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Genre(models.Model):
+    "Модель жанров."
+    pass
+
+
+class Category(models.Model):
+    "Модель категорий."
+    pass
+
+
+class Title(models.Model):
+    "Модель произведений."
+    pass
+
+
+class Review(models.Model):
+    "Модель отзывов."
+    pass
+
+
+class Comment(models.Model):
+    "Модель комментариев."
+    pass
