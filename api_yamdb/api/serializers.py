d9acb12d5839b9d485b5c8c07e391f0979a5b938
Мария Катаринска <katarinska@yandex.ru>
2022-11-01 14:30:06 +0300

    serializers

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+
+from .models import Comment, Review
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = (
+            'id', 'text', 'author',
+            'pub_date',
+        )
+        read_only_fields = (
+            'id', 'author', 'pub_date',
+        )
+        model = Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = (
+            'id', 'text', 'author',
+            'score', 'pub_date',
+        )
+        read_only_fields = (
+            'id', 'author', 'pub_date',
+        )
+        model = Review
+
+    def validate(self, data):
+        if self.context['request'].method == 'POST':
+            user = self.context['request'].user
+            title_id = self.context['view'].kwargs.get('title_id')
+            if Review.objects.filter(author=user, title_id=title_id).exists():
+                raise serializers.ValidationError('Вы уже оставили отзыв.')
+        return data
