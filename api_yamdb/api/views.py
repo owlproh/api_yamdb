d49f4650ed517de20d08caa3531ecdfa2ab16517
Святослав Прохоренко <prosvo@yandex.ru>
2022-11-03 00:38:39 +0300

    Начал писать views

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, permissions, status, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from reviews.models import Category, Genre, Title, Review, Comment
+from .serializers import (CategorySerializer, CommentSerializer,
+                          GenreSerializer, ReviewSerializer, TitleSerializer)
+
+
+class CategoryViewSet(mixins.CreateModelMixin,
+                      mixins.ListModelMixin,
+                      viewsets.GenericViewSet):
+    """Viewset для объектов модели Category."""
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class GenreViewSet(mixins.CreateModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
+    """Viewset для объектов модели Genre."""
+    queryset = Genre.objacts.all()
+    serializer_class = GenreSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
+
+
+class TitleViewSet(viewsets.ModelViewSet):
+    """Viewset для объектов модели Title."""
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer    
