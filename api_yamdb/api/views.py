fe9d8624dfb22106c1fa880c264d4ba3202d4940
Святослав Прохоренко <prosvo@yandex.ru>
2022-11-03 23:14:46 +0300

    some_fixes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,4 +1,6 @@
+from django.db.models import Avg
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.response import Response
 
@@ -21,7 +23,7 @@ class GenreViewSet(mixins.CreateModelMixin,
                    mixins.ListModelMixin,
                    viewsets.GenericViewSet):
     """Viewset для объектов модели Genre."""
-    queryset = Genre.objacts.all()
+    queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
@@ -29,5 +31,6 @@ class GenreViewSet(mixins.CreateModelMixin,
 
 class TitleViewSet(viewsets.ModelViewSet):
     """Viewset для объектов модели Title."""
-    queryset = Title.objects.all()
-    serializer_class = TitleSerializer    
+    queryset = Title.objects.annotate(rating=Avg('reviews__score'))
+    serializer_class = TitleSerializer
+    filter_backends = (DjangoFilterBackend,)
