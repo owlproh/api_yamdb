f42183f10900d896e50c8eda7d8c230a31604dce
Святослав Прохоренко <prosvo@yandex.ru>
2022-11-04 22:38:13 +0300

    redacted_permissions

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,28 +1,32 @@
 from rest_framework.permissions import (BasePermission,
                                         SAFE_METHODS)
 
+class IsSuperUser(BasePermission):  # проверку на superuser надо отдельно, это всё же не админ, а отдельный тип
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_superuser
+
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_admin
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):  # думаю не нужно на уровне объекта ещё проверять
         return request.user.is_authenticated and request.user.is_admin
 
 
 class IsAuthor(BasePermission):
-    def has_permission(self, request, view):
+    def has_permission(self, request, view):    # а это думаю не надо, автор всё же к объекту привязан
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user
+        return request.user.is_authenticated and obj.author == request.user  # на аутентификацию проверку надо бы тоже
 
 
 class IsModerator(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_moderator
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):  # думаю не нужно на уровне объекта ещё проверять
         return request.user.is_authenticated and request.user.is_moderator
 
 
@@ -30,5 +34,5 @@ class IsReadOnly(BasePermission):
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj):    # думаю не нужно на уровне объекта ещё проверять
         return request.method in SAFE_METHODS
