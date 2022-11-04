fb29b982f520ba754ee37b3b9a653e3fb9dfdcbb
Святослав Прохоренко <prosvo@yandex.ru>
2022-11-04 23:13:47 +0300

    users_changes

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -4,46 +4,20 @@ from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
 
-from .enums import UserRoles
 
 class User(AbstractUser):
-    """Модель пользователей."""
-    username = models.CharField(
-        max_length=150,
-        verbose_name='Имя пользователя',
-        unique=True,
-        db_index=True,
-        validators=[RegexValidator(
-            regex=r'^[\w.@+-]+$',
-            message='Имя пользователя содержит недопустимый символ'
-        )]
+    roles = (
+        ('admin', 'Администратор'),
+        ('moderator', 'Модератор'),
+        ('user', 'Пользователь'),
     )
-    email = models.EmailField(
-        max_length=254,
-        verbose_name='email',
-        unique=True
-    )
-    first_name = models.CharField(
-        max_length=150,
-        verbose_name='Имя',
-        blank=True
-    )
-    last_name = models.CharField(
-        max_length=150,
-        verbose_name='Фамилия',
-        blank=True
-    )
-    bio = models.TextField(
-        verbose_name='Биография пользователя',
-        blank=True
-    )
-    role = models.CharField(
-        max_length=UserRoles.max_length_choices(),
-        verbose_name='Роль',
-        choices=UserRoles.choices(),
-        default=UserRoles.user.name
-    )
-    # confirmation_code = ?
+    username = models.CharField('Логин', max_length=200, unique=True)
+    email = models.EmailField('Email', max_length=150, unique=True)
+    first_name = models.CharField('Имя', max_length=100)
+    last_name = models.CharField('Фамилия', max_length=100)
+    bio = models.TextField('Биография', blank=True)
+    role = models.CharField(max_length=50, choices=roles)
+    confirmation_code = models.CharField('Код подтверждения', max_length=255, null=True)
 
     class Meta:
         verbose_name = 'Пользователь'
