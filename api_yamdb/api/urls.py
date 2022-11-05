3c67788415ef541317a021262ff1aeb37d0fc19f
Святослав Прохоренко <prosvo@yandex.ru>
2022-11-05 22:14:31 +0300

    fixing

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,39 +2,25 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                    ReviewViewSet, TitleViewSet, SignUpAPIView,
-                    TokenAPIView, UsersViewSet, UserMeView)
+                    ReviewViewSet, TitleViewSet, UsersViewSet, TokenAPIView)
 
-<<<<<<< HEAD
 router = routers.DefaultRouter()
 router.register('categories', CategoryViewSet, basename='categories')
 router.register('genres', GenreViewSet, basename='genres')
 router.register('titles', TitleViewSet, basename='titles')
 router.register(
-=======
-v1_router = routers.DefaultRouter()
-v1_router.register('categories', CategoryViewSet, basename='categories')
-v1_router.register('genres', GenreViewSet, basename='genres')
-v1_router.register('titles', TitleViewSet, basename='titles')
-v1_router.register(
->>>>>>> users_manipulation
     r'titles/(?P<title_id>\d+)/reviews',
     ReviewViewSet,
     basename='reviews'
 )
-v1_router.register(
+router.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
-v1_router.register('users', UsersViewSet, basename='users')
-v1_router.register('users/me', UserMeView, basename='me')
 
-
-app_name = 'api'
+app_name='api'
 
 urlpatterns = [
-    path('v1/', include(v1_router.urls)),
-    path('v1/auth/signup/', SignUpAPIView.as_view()),
-    path('v1/auth/token/', TokenAPIView.as_view()),
+    path('v1/', include(router.urls)),
 ]
