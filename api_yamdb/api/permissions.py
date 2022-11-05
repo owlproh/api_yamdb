7632a3e153f8d4c78f7f78b4cc8abf00fbf7a15b
Святослав Прохоренко <prosvo@yandex.ru>
2022-11-05 22:12:17 +0300

    some_fixes

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -3,16 +3,10 @@ from rest_framework import permissions
 
 class IsOnlyAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
-<<<<<<< HEAD
-        return (
-            request.user.role == 'admin' or request.user.is_superuser
-        ) and request.user.is_authenticated
-=======
         return request.method == permissions.SAFE_METHODS
 
     def has_object_permission(self, request, view, obj):
         return request.user.role == 'admin'
->>>>>>> users_manipulation
 
 
 class IsAdminModerAuthor(permissions.BasePermission):
