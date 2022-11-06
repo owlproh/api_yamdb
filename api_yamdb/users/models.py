c38b047ec258d1ad0f1aef94c045b128ad25b49b
Святослав Прохоренко <prosvo@yandex.ru>
2022-11-06 15:47:43 +0300

    redacted by redoc

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -8,13 +8,37 @@ class User(AbstractUser):
         ('moderator', 'Модератор'),
         ('user', 'Пользователь'),
     )
-    username = models.CharField('Логин', max_length=200, unique=True)
-    email = models.EmailField('Email', max_length=150, unique=True)
-    first_name = models.CharField('Имя', max_length=100)
-    last_name = models.CharField('Фамилия', max_length=100)
-    bio = models.TextField('Биография', blank=True)
-    role = models.CharField(max_length=50, choices=roles)
-    confirmation_code = models.CharField('Код подтверждения', max_length=255, null=True)
+    username = models.CharField(
+        'Логин',
+        max_length=150,
+        unique=True
+    )
+    email = models.EmailField(
+        'Email',
+        max_length=254,
+        unique=True
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150
+    )
+    bio = models.TextField(
+        'Биография',
+        blank=True
+    )
+    role = models.CharField(
+        max_length=14,
+        choices=roles
+    )
+    confirmation_code = models.CharField(
+        'Код подтверждения',
+        max_length=255,
+        null=True
+    )
 
     def __str__(self):
         return self.username
